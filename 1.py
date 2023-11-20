0e9f24bf5fd6bd74348321edc592136db78b7c56
shreya-ai21 <shreya.ai21@bmsce.ac.in>
2023-11-20 14:21:43 +0530

    start

---
+++ 1.py
@@ -0,0 +1,13 @@
+def largest(arr:list):
+    return max(list)
+
+# def largest(arr:list):
+#     ans=0
+#     for item in arr:
+#         if item>ans:
+#             ans=item
+#     return ans
+
+arr=[1,5,4,3,7,8,2,10,9]
+ans=largest(arr)
+print(ans)

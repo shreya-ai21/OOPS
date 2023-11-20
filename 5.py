3a9fa783870ba2923e6db881d21b27f079675335
shreya-ai21 <shreya.ai21@bmsce.ac.in>
2023-11-20 15:41:02 +0530

    ,l

--- 5.py
+++ 5.py
@@ -1,1 +1,19 @@
+def palindrome(name:str):
+    ans=[]
+    for i in range(len(name)):
+        for j in range(i+1,len(name)):
+            if name[i]==name[j]:
+                a=i
+                b=j
+                while a<b:
+                    if name[a]==name[b]:
+                        a+=1
+                        b-=1
+                    else:
+                        break
+                if a>=b:
+                    ans.append(name[i:j+1])
+    return ans
+                
 name=input("Enter a string: ")
+print(palindrome(name))

81c698ec97c2fcba53096ea5a699ab6ba2774488
shreya-ai21 <shreya.ai21@bmsce.ac.in>
2023-11-20 15:06:16 +0530

    z

--- 3.py
+++ 3.py
@@ -1,8 +1,14 @@
-email=input("Enter your email address:")
+def is_valid_email(email:str):
+    if '@' in email:
+        em_index=email.index('@')
+        if em_index!=0 and email[-1]!='@' and '.' in email[em_index+1::] and email[em_index+1].isalnum() and email[-1]!='.' and email[0].isalnum() and len(email)<=256:
+            print("True")
+        else:
+            print("False")
+    else:
+        print("False")
 
-em_index=email.index('@')
-if '@' in email and em_index!=0 and email[-1]!='@' and '.' in email[em_index+1::] and email[em_index+1].isalnum() and email[-1]!='.' and email[0].isalnum() and len(email)<=256:
-    print("True")
-else:
-    print("False")
+email=input("Enter your email address:")
+is_valid_email(email)
+
     
